c1870d862e1bee5b1e2d8eb819a3114e14dbe084
B3an5 <beantheo@gmail.com>
2019-02-14 17:56:19 -0600

    video

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger.js'
 
 class BurgerBuilder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ingredients: {
+        salad: 1,
+        bacon: 10,
+        cheese: 30,
+        meat: 50
+      }
+    }
+  }
+
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     )
