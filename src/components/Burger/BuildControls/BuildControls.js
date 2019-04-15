a9630d4f1048d6cb18e2454121c7147fe4ecd4a2
B3an5 <beantheo@gmail.com>
2019-04-15 14:33:57 -0600

    increase ingredients

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BuildControls(props) {
+
+  return (
+    <div>
+      <h1 onClick={props.increaseSalad}>increase salad</h1>
+    </div>
+  )
+}
+
+export default BuildControls
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,24 +2,51 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger.js'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls.js'
 
 class BurgerBuilder extends Component {
   constructor() {
     super();
-    this.state = {
-      ingredients: {
+    this.state = { 
         salad: 1,
-        bacon: 10,
-        cheese: 30,
-        meat: 50
-      }
+        bacon: 1,
+        cheese: 3,
+        meat: 5
+      
     }
+  }
+
+  increaseSalad = () => {
+    this.setState({
+      ingredients: {salad: this.state.salad++}
+    })
+  }
+
+  increaseBacon = () => {
+    this.setState({
+      bacon: this.state.bacon++
+    })
+  }
+
+  increaseCheese = () => {
+    this.setState({
+      cheese: this.state.cheese++
+    })
+  }
+
+  increaseMeat = () => {
+    this.setState({
+      meat: this.state.meat++
+    })
   }
 
   render() {
     return (
       <Aux>
-        <Burger ingredients={this.state.ingredients} />
+        <Burger 
+        ingredients={this.state} />
+        <BuildControls 
+        increaseSalad={this.increaseSalad} />
         <div>Build Controls</div>
       </Aux>
     )
